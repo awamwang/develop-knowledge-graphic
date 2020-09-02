19b7d1e8c997d095209aeab914d2153d74cdfd32
awam.m.wang <awam.m.wang@cn.pwc.com>
2020-09-02 16:45:14 +0800

    chore:readme构建

---
+++ build/readme.js
@@ -0,0 +1,89 @@
+const rra = require('recursive-readdir-async')
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+const { getDefaultSettings } = require('http2')
+const outputDir = path.resolve(__dirname, '../out')
+const u = require('./util')({
+  basePath: outputDir
+})
+
+const write = util.promisify(fs.writeFile)
+
+const options = {
+  mode: rra.TREE,
+  recursive: true,
+  stats: false,
+  ignoreFolders: true,
+  extensions: false,
+  deep: false,
+  realPath: true,
+  normalizePath: true,
+  include: [],
+  exclude: [],
+  readContent: false,
+  encoding: 'utf8'
+}
+
+function genCategory(tree) {
+  let res = `## 分类\n\n`
+
+  tree.forEach((dir) => {
+    res += `+ [${dir.name}](${u.genHashLink(dir.name)})\n`
+  })
+
+  return res
+}
+
+function genDetailItems(dir, indentation = 0) {
+  return dir.content.reduce((prev, sub) => {
+    // name: '网络',
+    // path: 'C:/00awam/Repos/develop-knowledge-graphic/out',
+    // fullname: 'C:/00awam/Repos/develop-knowledge-graphic/out/网络',
+    // isDirectory: true,
+    // content: [ [Object] ]
+    let item =
+      '  '.repeat(indentation) +
+      `+ [${sub.name}](${u.getGitUrl(u.getRelativePath(sub.fullname))})\n`
+
+    if (sub.isDirectory) {
+      return prev + item + genDetailItems(sub, indentation + 1)
+    } else {
+      console.log(
+        sub.name,
+        sub.name.endsWith('.png'),
+        sub.name.endsWith('.png') ? item : ''
+      )
+      return prev + (sub.name.endsWith('.png') ? item : '')
+    }
+  }, '')
+}
+
+function genDetails(tree) {
+  let res = `## 具体内容\n\n`
+
+  tree.forEach((dir) => {
+    res += `### ${dir.name}\n\n`
+
+    res += genDetailItems(dir)
+
+    res += '\n'
+  })
+
+  return res
+}
+
+rra.list(outputDir, options).then((tree) => {
+  let readme = `# 开发知识\n\n`
+  console.log(tree)
+  readme += genCategory(tree)
+  readme += '\n'
+  readme += genDetails(tree)
+
+  // write(path.resolve(__dirname, '.temp\\readmeBody.md'), readme, {
+  //   encoding: 'utf-8'
+  // })
+  write(path.resolve(__dirname, '../README.md'), readme, {
+    encoding: 'utf-8'
+  })
+})
---
+++ build/util.js
@@ -0,0 +1,33 @@
+const path = require('path').posix
+
+const utilDefaultOptions = {}
+
+class Util {
+  gitBaseUlr =
+    'https://github.com/keepgoingwm/develop-knowledge-graphic/tree/master/out'
+
+  constructor(
+    { gitBaseUlr, basePath } = JSON.parse(JSON.stringify(utilDefaultOptions))
+  ) {
+    gitBaseUlr && (this.gitBaseUlr = gitBaseUlr)
+    basePath && (this.basePath = basePath)
+  }
+
+  genHashLink(name) {
+    return `#${name}`
+  }
+
+  getGitUrl(relativePath) {
+    return path.join(this.gitBaseUlr, relativePath.replace('\\', '/'))
+  }
+
+  getRelativePath(fullPath, basePath) {
+    return fullPath.replace(
+      (basePath || this.basePath || '').replace(/\\/g, '/'),
+      ''
+    )
+  }
+}
+module.exports = function (options) {
+  return new Util(options)
+}
